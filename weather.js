c6f36e8ef24f4368f21d64f5eb7f6fcd96ed293e
Shakil Ahmed <shakilahmed0888@gmail.com>
2023-12-19 19:35:04 +0530

    fixed weather data units in weather.js

--- weather.js
+++ weather.js
@@ -6,22 +6,21 @@ var key = process.env.WEATHER_API;
 
 const weatherReport = async (location) => {
   var link = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
-  // const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`, {
-  //           rejectUnauthorized: false,
-  //         });
   try {
     var response = await axios.get(link);
-    // console.log("response = ", response.data);
+    console.log("response = ", response.data);
     var weatherData = response.data;
     var city = weatherData.name;
-    var temp = weatherData.main.temp;
-    var weatherCondition = weatherData.main.humidity;
-    var region = weatherData.sys.country;
+    var temp = Math.round(weatherData.main.temp - 273.15, 2)
     var country = weatherData.sys.country;
     var windspeed = weatherData.wind.speed;
+    var weatherDescription = weatherData.weather[0].description;
     var humidity = weatherData.main.humidity;
-    var weathermsg = `The temperature in ${city} is ${temp}°C with ${weatherCondition} Sky\n Wind is blowing at a speed of ${windspeed} km/h \n humidity is ${humidity}. in ${city}, ${country}.`;
-    return weathermsg;
+    var pressure = Math.round(weatherData.main.pressure - 1013.15)
+    var sunrise = new Date(weatherData.sys.sunrise*1000);
+    var sunset = new Date(weatherData.sys.sunset*1000);
+    var message = `*** ${city} ***\nTemperature : ${temp}°C\nWind Speed: ${windspeed}m/s\nWeather : ${weatherDescription} \nHumidity: ${humidity}%\nPressure: ${pressure}atm\nSunrise : ${sunrise.toLocaleTimeString()}\nSunset : ${sunset.toLocaleTimeString()}\nCountry : ${country}`
+    return message;
   } catch (error) {
     console.error(error);
     return `sorry i don't know ${location}`;
